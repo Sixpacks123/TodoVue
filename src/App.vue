a9677f17d418c9c7214919b58e67fbbe336258f1
Sixpacks123 <aub.heurtault@gmail.com>
2021-10-22 11:12:27 +0200

    initialisation et création de deuc componements

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <AddTask/>
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AddTask from "./components/AddTask.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddTask
   }
 }
 </script>
---
+++ src/components/AddTask.vue
@@ -0,0 +1,53 @@
+<!--Addtask-->
+<template>
+<div>
+    <div id="AddTask">
+        <div class="row" style="margin-top:100px;">
+            <div class="col-1 col-md1 col-lg-2"></div>
+            <div class="col-10 col-md-9 col-lg-7" >
+                <div class="line">
+                    <input
+                    type = "text"
+                    class="text-line"
+                    placeholder="Add task here"
+                    v-model="newTodo"
+                    @keyup.enter="addTodo"
+                    maxlength="30"
+                    />
+                </div>
+                <div class="col-2 col-md-3"></div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+
+<script>
+export default {
+    name : "AddTask",
+
+    //return Data 
+    data(){
+        return{
+            todos: todoStorage.fetch(),
+            newTodo : ""
+        }
+    },methods: {
+        addTodo(){
+            const todoValue = this.newTodo && this.newTodo.trim()
+            if(!todoValue){
+                return;
+            }
+            this.todos.push({
+                id: todoStorage.uid++,
+                title : todoValue,
+                favourite : false,
+                completed : false
+            });
+
+            this.newTodo = " "
+        }
+    }
+}
+</script>
